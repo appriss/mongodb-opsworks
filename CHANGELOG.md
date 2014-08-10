425564fc5770834b1afc9d2bb3cd714c10db5adf
Donavan Pantke <dpantke@appriss.com>
2014-08-10 18:56:39 -0400

    Add Berkshelf support and throw in some debugging.

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+mongodb-opsworks CHANGELOG
+==========================
+
+This file is used to list changes made in each version of the mongodb-opsworks cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of mongodb-opsworks
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ recipes/default.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: mongodb-opsworks
+# Recipe:: default
+#
+# Copyright 2014, Appriss, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node['opsworks']['instance']['layers'].each do |layer|
+	if Regexp.new(node['opsworks-mongodb']['replset_pattern']).match(layer)
+		node.normal['mongodb']['shard_name'] = $1
+		node.normal['mongodb']['config']['replSet'] = $1
+	end
+end
+
+node_overrides = node['opsworks-mongodb']['instance_overrides'][node['opsworks']['instance']['hostname']]
+
+
+total = Array.new
+node['mongodb'].each_attribute do |k,v|
+	line = "k: #{k}, v:#{v}"
+	total.push line
+end
+
+execute "Output node list" do
+	command "echo #{total} >>/tmp/total.out"
+end
+
+
+if node_overrides
+	node_overrides.each_attribute do |key, val|
+		node.override['mongodb'][key] = val
+	end
+end
