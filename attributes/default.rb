00ece855ff5e4e05bfc5d34a243174de9b41626b
Donavan Pantke <dpantke@appriss.com>
2014-08-11 09:53:33 -0400

    Hopefully create data bag out of OpsWorks config that the mongodb cookbook can use.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['opsworks-mongodb']['replset_layer_pattern'] = '^mongo-replicaset-(\S+)$'
 default['opsworks-mongodb']['configsvr_layer'] = 'mongo-configsvr'
 default['opsworks-mongodb']['mongos_layer'] = 'mongo-mongos'
+default['opsworks-mongodb']['sharded'] = false
 # Start with an empty node override section, this allows the user to use the stack JSON to override attributes (such as node priority) on an individual instance.
 default['opsworks-mongodb']['instance_overrides'] = ""
--- recipes/default.rb
+++ recipes/default.rb
@@ -18,22 +18,65 @@ node['opsworks']['instance']['layers'].each do |layer|
 	end
 end
 
+def init_item(instance_name,instance_config)
+	#Deep copy node set
+	instance_item = Marshal.load(Marshal.dump(instance_config))
+	instance_item['id'] = instance_name
+	overrides = node['opsworks-mongodb']['instance_overrides'][instance_name]
+	if overrides
+		Chef::Mixin::DeepMerge(instance_item,overrides)
+	end
+	return instance_item
+end
+
+def save_item(item)
+	db_item = Chef::DataBagItem.new
+	db_item.data_bag = "cluster_config"
+	db_item.raw_data = item.to_json
+	db_item.save
+end
+
+cluster_config = Chef::Databag.new
+cluster_config.name("cluster_config")
+node['opsworks']['layers'].each_attribute do |layer,config|
+	layer_name = layer['name']
+	case 
+	when Regexp.new(node['opsworks-mongodb']['replset_layer_pattern']).match(layer_name)
+		shard_or_replset_name = $1
+		node['opsworks']['layers'][layer]['instances'].each_attribute do |instance,config|
+			item = init_item(instance,config)
+			item['mongodb']['is_replicaset'] = true
+			if node['opsworks-mongodb']['sharded']
+				item['mongodb']['is_shard'] = true
+				item['mongodb']['shard_name'] = shard_or_replset_name
+			else
+				item['mongodb']['config']['replSet'] = shard_or_replset_name
+			end
+		end
+		save_item(item)
+	when layer_name == node['opsworks-mongodb']['configsvr_layer']
+		node['opsworks']['layers'][layer]['instances'].each_attribute do |instance,config|
+			item = init_item(instance,config)
+			item['mongodb']['is_configserver'] = true
+			save_item
+		end
+	when layer_name == node['opsworks-mongodb']['mongos_layer']
+		node['opsworks']['layers'][layer]['instances'].each_attribute do |instance,config|
+			item = init_item(instance,config)
+			item['mongodb']['is_mongos'] = true
+			item['mongodb']['config']['instance_name'] = "mongos"
+		end
+	end
+end
+
+Chef::Log.info('Databag: #{cluster_config}')
+
+
+
 node_overrides = node['opsworks-mongodb']['instance_overrides'][node['opsworks']['instance']['hostname']]
-
-
-total = Array.new
-node.each_attribute do |k,v|
-	line = "k: #{k}, v:#{v}"
-	total.push line
-end
-
-file "/tmp/total.out" do
-	content total.to_s
-end
-
 
 if node_overrides
 	node_overrides.each_attribute do |key, val|
-		node.override['mongodb'][key] = val
+		Chef::Mixin::DeepMerge(instance_item,overrides)
 	end
 end
