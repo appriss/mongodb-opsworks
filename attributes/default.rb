25519e2bf79bb7b189a9723e14d6b97ed5b9cb62
Donavan Pantke <dpantke@appriss.com>
2014-08-10 20:40:10 -0400

    Change pattern default value

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['opsworks-mongodb']['replset_layer_pattern'] = '^mongo-replset-(\S+)$'
+default['opsworks-mongodb']['replset_layer_pattern'] = '^mongo-replicaset-(\S+)$'
 default['opsworks-mongodb']['configsvr_layer'] = 'mongo-configsvr'
 default['opsworks-mongodb']['mongos_layer'] = 'mongo-mongos'
 # Start with an empty node override section, this allows the user to use the stack JSON to override attributes (such as node priority) on an individual instance.
