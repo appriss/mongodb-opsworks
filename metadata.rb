7ca9192ac2cd415ca106f22b51278be30733c426
Donavan Pantke <dpantke@appriss.com>
2014-08-10 20:01:50 -0400

    Depends not requires. duh.

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures mongodb-opsworks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-requires 'mongodb'
+depends 'mongodb'
