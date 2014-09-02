9eaabd705dc593c81b21a45536e471d3e6b94185
Donavan Pantke <dpantke@appriss.com>
2014-09-02 00:54:52 -0400

    Mark version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dpantke@appriss.com'
 license          'Apache 2.0'
 description      'Prepares an OpsWorks stack to run the chef-mongodb recipe'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends 'mongodb'
